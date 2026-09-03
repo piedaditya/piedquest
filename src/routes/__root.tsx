43c6990c0d7fc7351cce7749797242f2a946b9c1
piedaditya <piedaditya@gmail.com>
2026-09-03 21:17:41 +0530

    Remove global ad script from __root.tsx
    Removed Monetag Global Ad Script from the head section.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -114,8 +114,6 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        {/* Monetag Global Ad Script */}
-        <script src="https://quge5.com/88/tag.min.js" data-zone="274315" async data-cfasync="false"></script>
       </head>
       <body>
         {children}
